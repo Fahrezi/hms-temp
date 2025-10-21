805824e6abe94809d5518c96c3f0ee46e9e48765
Muhammad Luthfi Fahrezi <lutfhi.fahrezi@weave.co.id>
2025-10-21 23:00:59 +0700

    fix: no PWA

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 import { defineConfig } from 'vite';
 // eslint-disable-next-line import/no-named-as-default
 import checker from 'vite-plugin-checker';
-import { VitePWA } from 'vite-plugin-pwa';
+// import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -16,28 +16,28 @@ export default defineConfig({
         tsconfigPath: './tsconfig.app.json',
       },
     }),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
-      manifest: {
-        name: 'Hotel Management System',
-        short_name: 'HMS',
-        description: 'Hotel Management System - New System',
-        theme_color: '#ffffff',
-        icons: [
-          {
-            src: '/images/pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: '/images/pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-        ],
-      },
-    }),
+    // VitePWA({
+    //   registerType: 'autoUpdate',
+    //   includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+    //   manifest: {
+    //     name: 'Hotel Management System',
+    //     short_name: 'HMS',
+    //     description: 'Hotel Management System - New System',
+    //     theme_color: '#ffffff',
+    //     icons: [
+    //       {
+    //         src: '/images/pwa-192x192.png',
+    //         sizes: '192x192',
+    //         type: 'image/png',
+    //       },
+    //       {
+    //         src: '/images/pwa-512x512.png',
+    //         sizes: '512x512',
+    //         type: 'image/png',
+    //       },
+    //     ],
+    //   },
+    // }),
   ],
   resolve: {
     alias: {
