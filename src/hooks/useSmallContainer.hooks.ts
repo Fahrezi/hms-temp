3ecb020fe600dc10326eb488081ae025dcba464b
Muhammad Luthfi Fahrezi <lutfhi.fahrezi@weave.co.id>
2026-01-23 04:40:42 +0700

    hide enable small container

--- src/hooks/useSmallContainer.hooks.ts
+++ src/hooks/useSmallContainer.hooks.ts
@@ -1,13 +1,13 @@
 import { useEffect } from 'react';
 
-import getEnvValue from '@/utils/env';
-import { ENVIRONMENT } from '@/constants/envs';
+// import getEnvValue from '@/utils/env';
+// import { ENVIRONMENT } from '@/constants/envs';
 
 const SMALL_CONTAINER_CLASS = 'small-container';
 
 const useSmallContainer = () => {
   useEffect(() => {
-    const isSmallContainer = getEnvValue(ENVIRONMENT.ENABLE_SMALL_CONTAINER) === 'true';
+    const isSmallContainer = true;
 
     if (isSmallContainer) {
       document.body.classList.add(SMALL_CONTAINER_CLASS);
